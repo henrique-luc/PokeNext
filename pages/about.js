4520f4618583c3863a643a8e010e2fb61ccd2f4f
Henrique Luciano <henrique.luc@outlook.com>
2022-08-29 16:12:25 -0300

    feat: aplication

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+import styles from "../styles/About.module.css";
+
+export default function About() {
+  return (
+    <div className={styles.about}>
+      <h1>Sobre o projeto</h1>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor sit
+        amet lorem ipsum dolor amet
+      </p>
+      <Image
+        src="/images/charizard.png"
+        width="300"
+        height="300"
+        alt="Charizard"
+      />
+    </div>
+  );
+}
